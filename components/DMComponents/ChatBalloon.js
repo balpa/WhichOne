7c5765f97814480bbbfd0fe08ac0c9043a17cd27
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-06-10 00:23:02 +0300

    fetch dm messages. need styling balloons etc.

---
+++ components/DMComponents/ChatBalloon.js
@@ -0,0 +1,30 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import { registerVersion } from 'firebase/app'
+
+const ChatBalloon = ({ message, sender }) => {
+  return (
+    <View style={[styles.container, sender == 'loggedinuser' ? {flexGrow: 1} : {}]}>
+      <Text style={{
+        color:'white',
+        textAlign:'center',
+        fontSize:15,
+        fontWeight:'600'
+      }}>{message}</Text>
+    </View>
+  )
+}
+
+export default ChatBalloon
+
+
+const styles = StyleSheet.create({
+  container: {
+
+    width: 100,
+    minHeight: 50,
+    backgroundColor:'green',
+    margin: 10,
+  },
+
+})
--- pages/DMChatPage.js
+++ pages/DMChatPage.js
@@ -5,6 +5,7 @@ import { ScrollView } from 'react-native-gesture-handler'
 import { Input } from 'react-native-elements'
 import { doc, onSnapshot, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import useKeyboardHeight from 'react-native-use-keyboard-height'
+import ChatBalloon from '../components/DMComponents/ChatBalloon'
 
 const DMChatPage = ({ route, navigation }) => {
 
@@ -12,6 +13,7 @@ const DMChatPage = ({ route, navigation }) => {
 
   const [isEmpty, setIsEmpty] = React.useState(false)
   const [messageText, setMessageText] = React.useState('')
+  const [messageData, setMessageData] = React.useState(null)
 
   const { userID, name, userData } = route.params
   const loggedinUser = auth.currentUser
@@ -28,6 +30,13 @@ const DMChatPage = ({ route, navigation }) => {
     }).start()
   }, [ref, keyboardHeight])
 
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, "messages",`bw${loggedinUser.uid}and${userID}` ), (doc) => {
+      setMessageData(doc.data())
+  })
+ }, [])
+
+ // console.log(Object.keys(messageData.data).map(key => messageData.data[key].message))
 
   async function sendMessage(){
 
@@ -40,20 +49,22 @@ const DMChatPage = ({ route, navigation }) => {
         timestamp: date, 
       }}
         await setDoc(doc(db,'messages', `bw${loggedinUser.uid}and${userID}`), {data},{merge: true})
-      } else {
-          alert("Message must be between 1 and 150 characters")
-      }
+      } else alert("Message must be between 1 and 150 characters")
     }
 
   return (
     <View style={styles.container}>
       <ScrollView style={styles.chatMessagesContainer}>
-        <Text>Chatting with {name}</Text>
-        <Text>Chatting with {name}</Text>
-        <Text>Chatting with {name}</Text>
-        <Text>Chatting with {name}</Text>
-        <Text>Chatting with {name}</Text>
-        <Text>Chatting with {name}</Text>
+        {messageData != null ? Object.keys(messageData.data).map(key => {
+          if (messageData.data[key].sender === loggedinUser.uid) {
+            return (
+              <ChatBalloon message={messageData.data[key].message} sender={"loggedinuser"} />
+            )}
+          else {
+            return (
+              <ChatBalloon message={messageData.data[key].message} sender={"otheruser"} />
+            )}
+        }) : null}
       </ScrollView>
       <Animated.View style={[styles.messageInputContainer, {bottom: inputAnim}]}>
         <Input  
@@ -77,6 +88,7 @@ const styles = StyleSheet.create({
   chatMessagesContainer: {
     width:'100%',
     height:'80%',
+    flexDirection:'column',
   },
   messageInputContainer: {
     width:'100%',
