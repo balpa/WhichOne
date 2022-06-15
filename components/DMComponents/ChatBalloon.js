87dcd3a535044fbed02afb7ecc43f1866fd1d8f6
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-06-15 16:56:10 +0300

    chat balloon detailed date

--- components/DMComponents/ChatBalloon.js
+++ components/DMComponents/ChatBalloon.js
@@ -2,18 +2,23 @@ import { View, Text, StyleSheet } from 'react-native'
 import React, { useEffect } from 'react'
 import { registerVersion } from 'firebase/app'
 import { auth, db } from '../../firebase'
+import { TouchableOpacity } from 'react-native-gesture-handler'
 
 const ChatBalloon = ({ message, sender, item }) => {
 
   const loggedinUser = auth.currentUser
 
   const [senderPosition, setSenderPosition] = React.useState(null)
-  const [timeForChatBalloon, setTimeForChatBalloon] = React.useState("")
   const [borderSettings, setBorderSettings] = React.useState({})
+  const [timeForChatBalloon, setTimeForChatBalloon] = React.useState("")
+  const [detailedTimeForChatBalloon, setDetailedTimeForChatBalloon] = React.useState("")
+  const [showDetailedDate, setShowDetailedDate] = React.useState(false)
 
   function dateAdjuster(dateFromItem){
     let forAdjust = new Date(dateFromItem).toString()
     let dateArray = forAdjust.split(' ')
+    let detailedDate = dateArray[0] + ", " + dateArray[1] + " " + dateArray[2] + ", " + dateArray[3] + " " + dateArray[4].slice(0,5)
+    setDetailedTimeForChatBalloon(detailedDate)
     setTimeForChatBalloon(dateArray[4].slice(0,5))
   }
 
@@ -43,9 +48,16 @@ const ChatBalloon = ({ message, sender, item }) => {
         position:'absolute',
         bottom: -15,
         right: 0,
-
       }}>
-        <Text style={{fontSize: 8, fontWeight:'700'}}>{timeForChatBalloon}</Text>
+        <TouchableOpacity 
+          style={{
+            width:'100%',
+            height:'100%'
+          }}
+          onPress={() => setShowDetailedDate(!showDetailedDate)}
+          >
+          <Text style={{fontSize: 8, fontWeight:'700'}}>{showDetailedDate == false ? timeForChatBalloon : detailedTimeForChatBalloon}</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
