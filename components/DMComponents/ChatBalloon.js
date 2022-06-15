ba689338dbed19925a063ee9cfa253090ec1a1dc
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-06-15 11:48:28 +0300

    chat balloon styling and date added

--- components/DMComponents/ChatBalloon.js
+++ components/DMComponents/ChatBalloon.js
@@ -8,23 +8,45 @@ const ChatBalloon = ({ message, sender, item }) => {
   const loggedinUser = auth.currentUser
 
   const [senderPosition, setSenderPosition] = React.useState(null)
+  const [timeForChatBalloon, setTimeForChatBalloon] = React.useState("")
+  const [borderSettings, setBorderSettings] = React.useState({})
+
+  function dateAdjuster(dateFromItem){
+    let forAdjust = new Date(dateFromItem).toString()
+    let dateArray = forAdjust.split(' ')
+    setTimeForChatBalloon(dateArray[4].slice(0,5))
+  }
+
+  useEffect(() => {
+    dateAdjuster(item.time)
+  }, [])
 
   useEffect(() => {
     if (item.sender === loggedinUser.uid) {
       setSenderPosition('flex-end')
+      setBorderSettings({ borderTopLeftRadius: 15, borderBottomLeftRadius: 15 })
     } else {
       setSenderPosition('flex-start')
+      setBorderSettings({ borderTopRightRadius: 15, borderBottomRightRadius: 15 })
     }
   }, [])
 
   return (
-    <View style={[styles.container, {alignSelf: senderPosition}]}>
+    <View style={[styles.container, borderSettings, {alignSelf: senderPosition}]}>
       <Text style={{
         color:'white',
         textAlign:'center',
         fontSize:15,
         fontWeight:'600'
       }}>{item.message}</Text>
+      <View style={{
+        position:'absolute',
+        bottom: -15,
+        right: 0,
+
+      }}>
+        <Text style={{fontSize: 8, fontWeight:'700'}}>{timeForChatBalloon}</Text>
+      </View>
     </View>
   )
 }
@@ -39,7 +61,7 @@ const styles = StyleSheet.create({
     minHeight: 50,
     backgroundColor:'green',
     margin: 10,
-    borderRadius:15,
+
   },
 
 })
