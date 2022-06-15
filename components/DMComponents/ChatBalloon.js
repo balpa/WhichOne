1cd1d9ca89f029be146868238f29fc3fc39fdccf
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-06-15 17:07:45 +0300

    chat balloon animation

--- components/DMComponents/ChatBalloon.js
+++ components/DMComponents/ChatBalloon.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Animated } from 'react-native'
 import React, { useEffect } from 'react'
 import { registerVersion } from 'firebase/app'
 import { auth, db } from '../../firebase'
@@ -8,13 +8,15 @@ const ChatBalloon = ({ message, sender, item }) => {
 
   const loggedinUser = auth.currentUser
 
+  const chatBalloonAnimation = item.sender === loggedinUser.uid ?  React.useRef(new Animated.Value(300)).current : React.useRef(new Animated.Value(-300)).current
+
   const [senderPosition, setSenderPosition] = React.useState(null)
   const [borderSettings, setBorderSettings] = React.useState({})
   const [timeForChatBalloon, setTimeForChatBalloon] = React.useState("")
   const [detailedTimeForChatBalloon, setDetailedTimeForChatBalloon] = React.useState("")
   const [showDetailedDate, setShowDetailedDate] = React.useState(false)
 
-  function dateAdjuster(dateFromItem){
+  function dateAdjuster(dateFromItem){        // date adjust func
     let forAdjust = new Date(dateFromItem).toString()
     let dateArray = forAdjust.split(' ')
     let detailedDate = dateArray[0] + ", " + dateArray[1] + " " + dateArray[2] + ", " + dateArray[3] + " " + dateArray[4].slice(0,5)
@@ -22,7 +24,15 @@ const ChatBalloon = ({ message, sender, item }) => {
     setTimeForChatBalloon(dateArray[4].slice(0,5))
   }
 
-  useEffect(() => {
+  useEffect(() => {       // chat balloon animation with conditions
+      Animated.timing(chatBalloonAnimation, {
+        toValue: 0,
+        duration: 400,
+        useNativeDriver: false,
+      }).start()
+  }, [])
+
+  useEffect(() => {         // on first render, run date adjuster func
     dateAdjuster(item.time)
   }, [])
 
@@ -37,7 +47,7 @@ const ChatBalloon = ({ message, sender, item }) => {
   }, [])
 
   return (
-    <View style={[styles.container, borderSettings, {alignSelf: senderPosition}]}>
+    <Animated.View style={[styles.container, borderSettings, {alignSelf: senderPosition}, {transform: [{translateX: chatBalloonAnimation}]}]}>
       <Text style={{
         color:'white',
         textAlign:'center',
@@ -59,7 +69,7 @@ const ChatBalloon = ({ message, sender, item }) => {
           <Text style={{fontSize: 8, fontWeight:'700'}}>{showDetailedDate == false ? timeForChatBalloon : detailedTimeForChatBalloon}</Text>
         </TouchableOpacity>
       </View>
-    </View>
+    </Animated.View>
   )
 }
 
