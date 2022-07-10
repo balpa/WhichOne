27fe552040f556a23994c506537a5b92f7e53613
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-07-10 20:49:07 +0300

    about page dark mode

--- pages/AboutPage.js
+++ pages/AboutPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { View, Text, StyleSheet, Alert } from 'react-native'
 import { Button, Image, Input } from "react-native-elements"
@@ -7,16 +7,33 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StackActions } from '@react-navigation/native';
 import { auth } from '../firebase'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ScrollView } from 'react-native-gesture-handler'
 
 const AboutPage = ({ navigation }) => {
 
+    const [selectedTheme, setSelectedTheme] = useState('')
+    const [textColorDependingOnTheme, setTextColorDependingOnTheme] = useState('')
+
+    useEffect(async()=>{      // get theme data from local storage (cache) ***HARDCODED***
+        try {
+          const value = await AsyncStorage.getItem('GLOBAL_THEME')
+          if(value !== null) {
+            setSelectedTheme(value)
+            if (value == 'light') setTextColorDependingOnTheme('black')
+            else setTextColorDependingOnTheme('white')}
+        } catch(e) {console.log(e)}
+      },[])
+
     return (
         <>
-        <View style={styles.container}>
-            <View style={styles.elevation}>
-                <Text>baLpa</Text>
-            </View>
+        <View style={[
+            styles.container,
+            selectedTheme == 'dark' ? {backgroundColor:'rgb(15,15,15)'} : {backgroundColor:'white'}
+            ]}>
+            <Text>
+                asbout
+            </Text>
         </View>
     
         </>
@@ -40,22 +57,10 @@ const styles = StyleSheet.create({
         alignItems:"center",
         justifyContent: "center",
         backgroundColor: "rgba(15,15,15,1)",
-        borderWidth: 1,
         borderBottomLeftRadius: 25,
         borderBottomRightRadius: 25 
     },
-    elevation:{
-        width: 300,
-        height: 500,
-        justifyContent: "center", alignItems: "center", 
-        backgroundColor: "rgba(255,255,255,0.1)",
-        shadowColor: '#000',
-        shadowOffset: { width: 1, height: 3 },
-        shadowOpacity: 0.9,
-        shadowRadius: 2,
-        elevation: 5,
-        borderRadius: 40,
-    },
+   
     profileBody: {
     }
 
